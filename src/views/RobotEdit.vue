9cd741a50b1c093e8be6a4486548326d5b605a8a
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-02-27 11:36:01 -0500

    added robot visualization

--- src/views/RobotEdit.vue
+++ src/views/RobotEdit.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="RobotEdit p-5">
+  <div class="RobotEdit p-5" v-if="robot">
     <h2 class="is-size-2 has-text-left">Edit Robot {{ this.$route.params.robotID }}</h2>
-    {{ robot }}
+
+    <h4 class="is-size-4 has-text-left"><strong>Name: </strong>{{ robot.name }}</h4>
+    <h4 class="is-size-4 has-text-left"><strong>Color: </strong>{{ robot.color }}</h4>
+    <h4 class="is-size-4 has-text-left"><strong>Attack: </strong>{{ robot.attack }}</h4>
+    <h4 class="is-size-4 has-text-left"><strong>Defense: </strong>{{ robot.defense }}</h4>
   </div>
 </template>
 
